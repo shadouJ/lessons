456e3c148a20b3bfeaa3b92b2d3bc35a03f1d238
eggdandanni <39125527+eggdandanni@users.noreply.github.com>
2019-07-08 13:35:01 +0930

    lecture: eight-queens
    lecture: eight-queens

---
+++ src/components/lessons/eight-queens/App.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <div v-if="selectedOption === -1">
+            <app-title-static>
+                Eight Queens
+            </app-title-static>
+            <transition appear appear-class="options-appear" appear-active-class="options-appear-active">
+                <app-options :options="options" @selectOption="selectedOption=$event"></app-options>
+            </transition>
+        </div>
+
+        <div class="container-fluid" v-else>
+            <transition appear appear-class="lesson-appear" appear-active-class="lesson-appear-active">
+                <app-top-nav @backToMenu="selectedOption = $event">Eight Queens</app-top-nav>
+            </transition>
+
+            <transition appear appear-class="lesson-appear" appear-active-class="lesson-appear-active">
+                <app-play-eight-queens v-if="selectedOption === 1"></app-play-eight-queens>
+                <app-marching-queens v-if="selectedOption === 2"></app-marching-queens>
+                <app-fast-computer-search v-if="selectedOption === 3"></app-fast-computer-search>
+            </transition>
+        </div>
+        <!-- <transition appear appear-class="options-appear" appear-active-class="options-appear-active">
+            <app-bottom-nav></app-bottom-nav>
+        </transition>
+    -->
+    </div>
+</template>
+
+<script>
+    // import Title from '../../layout/Title.vue';
+    import TitleStatic from '../../layout/TitleStatic.vue';
+    import TopNav from '../../layout/TopNav.vue';
+    import Options from '../../layout/Options.vue';
+    import PlayEightQueens from "./PlayEightQueens";
+    import FastComputerSearch from "./FastComputerSearch";
+    import MarchingQueens from "./MarchingQueens";
+
+
+    export default {
+        components: {
+            // appTitle: Title,
+            appTitleStatic: TitleStatic,
+            appTopNav: TopNav,
+            appOptions: Options,
+            // appBottomNav: BottomNav,
+            appPlayEightQueens: PlayEightQueens,
+            appMarchingQueens: MarchingQueens,
+            appFastComputerSearch:FastComputerSearch
+
+        },
+        data: function() {
+            return {
+                options: [
+                    { id: 1, title: 'Play Eight Queens' },
+                    { id: 2,title:'Marching Queens'},
+                    { id: 3,title:"Fast Computer Search"}
+
+                ],
+                selectedOption: -1,
+            }
+        }
+    }
+</script>
+
+<style>
+    .tt--right-box {
+        margin-bottom: 3rem;
+    }
+    .tt--right-row {
+        display: flex;
+    }
+
+
+</style>
