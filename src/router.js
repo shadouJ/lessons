0c89cd4d3c524c0b1498b1eb7a0a3290c8ff0c9d
Fan Yang <yangfcm16@gmail.com>
2019-07-11 14:50:24 +0930

    Use route lazy loading

--- src/router.js
+++ src/router.js
@@ -1,20 +1,97 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Trigonometry from './components/lessons/trigonometry/App.vue';
-import TimesTables from './components/lessons/times-tables/App.vue';
-import Truth1 from "./components/lessons/truth-tiles-1/App.vue";
-import Truth2 from "./components/lessons/truth-tiles-2/App.vue";
+// import Trigonometry from './components/lessons/trigonometry/App.vue';
+// import TimesTables from './components/lessons/times-tables/App.vue';
+// import Truth1 from "./components/lessons/truth-tiles-1/App.vue";
+// import Truth2 from "./components/lessons/truth-tiles-2/App.vue";
 import Home from './components/Home.vue';
-import TakeAwayTiles from "./components/lessons/take-away-tiles/App.vue";
-import Steps from "./components/lessons/steps/App.vue";
-import NumberTiles from "./components/lessons/number-tiles/App.vue";
-import ChessQueens from "./components/lessons/chess-queens/App.vue";
-import EightQueens from "./components/lessons/eight-queens/App.vue";
-import Crosses from "./components/lessons/crosses/App.vue";
-import AngleEstimation from './components/lessons/angle-estimation/App.vue';
-import AreaOfTriangle from './components/lessons/area-of-triangle/App.vue';
-import BiggestVolume from './components/lessons/biggest-volume/App.vue';
-import IceCreamFlavours from './components/lessons/ice-cream-flavours/App.vue';
+// import TakeAwayTiles from "./components/lessons/take-away-tiles/App.vue";
+// import Steps from "./components/lessons/steps/App.vue";
+// import NumberTiles from "./components/lessons/number-tiles/App.vue";
+// import ChessQueens from "./components/lessons/chess-queens/App.vue";
+// import EightQueens from "./components/lessons/eight-queens/App.vue";
+// import Crosses from "./components/lessons/crosses/App.vue";
+// import AngleEstimation from './components/lessons/angle-estimation/App.vue';
+// import AreaOfTriangle from './components/lessons/area-of-triangle/App.vue';
+// import BiggestVolume from './components/lessons/biggest-volume/App.vue';
+// import IceCreamFlavours from './components/lessons/ice-cream-flavours/App.vue';
+const Trigonometry = resolve => {
+  require.ensure('./components/lessons/trigonometry/App.vue', () => {
+    resolve(require('./components/lessons/trigonometry/App.vue'))
+  });
+}
+
+const TimesTables = resolve => {
+  require.ensure('./components/lessons/times-tables/App.vue', () => {
+    resolve(require('./components/lessons/times-tables/App.vue'))
+  });
+}
+
+const Truth1 = resolve => {
+  require.ensure('./components/lessons/truth-tiles-1/App.vue', () => {
+    resolve(require('./components/lessons/truth-tiles-1/App.vue'))
+  });
+}
+
+const Truth2 = resolve => {
+  require.ensure('./components/lessons/truth-tiles-2/App.vue', () => {
+    resolve(require('./components/lessons/truth-tiles-2/App.vue'))
+  });
+}
+
+const TakeAwayTiles = resolve => {
+  require.ensure('./components/lessons/take-away-tiles/App.vue', () => {
+    resolve(require('./components/lessons/take-away-tiles/App.vue'))
+  });
+}
+
+const Steps = resolve => {
+  require.ensure('./components/lessons/steps/App.vue', () => {
+    resolve(require('./components/lessons/steps/App.vue'))
+  });
+}
+
+const NumberTiles = resolve => {
+  require.ensure('./components/lessons/number-tiles/App.vue', () => {
+    resolve(require('./components/lessons/number-tiles/App.vue'))
+  });
+}
+
+const ChessQueens = resolve => {
+  require.ensure('./components/lessons/chess-queens/App.vue', () => {
+    resolve(require('./components/lessons/chess-queens/App.vue'))
+  });
+}
+
+const Crosses = resolve => {
+  require.ensure('./components/lessons/crosses/App.vue', () => {
+    resolve(require('./components/lessons/crosses/App.vue'))
+  });
+}
+
+const AngleEstimation = resolve => {
+  require.ensure('./components/lessons/angle-estimation/App.vue', () => {
+    resolve(require('./components/lessons/angle-estimation/App.vue'))
+  });
+}
+
+const AreaOfTriangle = resolve => {
+  require.ensure('./components/lessons/area-of-triangle/App.vue', () => {
+    resolve(require('./components/lessons/area-of-triangle/App.vue'))
+  });
+}
+
+const BiggestVolume = resolve => {
+  require.ensure('./components/lessons/biggest-volume/App.vue', () => {
+    resolve(require('./components/lessons/biggest-volume/App.vue'))
+  });
+}
+
+const IceCreamFlavours = resolve => {
+  require.ensure('./components/lessons/ice-cream-flavours/App.vue', () => {
+    resolve(require('./components/lessons/ice-cream-flavours/App.vue'))
+  });
+}
 
 Vue.use(VueRouter);
 
