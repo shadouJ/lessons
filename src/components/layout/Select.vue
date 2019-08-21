adbfc3680cd0843bdb4b664d83b7d8b15d6446c0
Fan Yang <yangfcm16@gmail.com>
2019-08-21 13:14:23 +0930

    Add Home button on lesson's main page

--- src/components/layout/Select.vue
+++ src/components/layout/Select.vue
@@ -1,11 +1,22 @@
 <template>
 	<div class="alert alert-dark app--select-text">
-		<h4><i class="fas fa-map-pin"></i>&nbsp;&nbsp;Select an option</h4></div>
+		<h4><i class="fas fa-map-pin"></i>&nbsp;&nbsp;Select an option</h4>
+		<div><router-link class="btn btn-outline-dark" to="/">
+			<i class="fas fa-home"></i>&nbsp;Home</router-link>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-	
 
 }
 </script>
+
+<style scoped>
+.app--select-text {
+	display: flex;
+	justify-content: space-between;
+}
+</style>
+
